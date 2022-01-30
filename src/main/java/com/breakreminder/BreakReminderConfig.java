53f244210791ded5fb9b36c811bc2304490397fe
LSO107 <34274877+LSO107@users.noreply.github.com>
2022-01-30 11:54:46 +0000

    rename example to breakreminder

--- src/main/java/com/breakreminder/BreakReminderConfig.java
+++ src/main/java/com/breakreminder/BreakReminderConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.breakreminder;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface BreakReminderConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/main/java/com/breakreminder/BreakReminderPlugin.java
+++ src/main/java/com/breakreminder/BreakReminderPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.breakreminder;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class BreakReminderPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private BreakReminderConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	BreakReminderConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(BreakReminderConfig.class);
 	}
 }
