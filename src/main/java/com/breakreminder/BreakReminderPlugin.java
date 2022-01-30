b12ea68b3a24f872c070e5a3a8782a3a256b537c
LSO107 <34274877+LSO107@users.noreply.github.com>
2022-01-30 19:12:36 +0000

    create plugin for managing break start/reset/stop

--- src/main/java/com/breakreminder/BreakReminderPlugin.java
+++ src/main/java/com/breakreminder/BreakReminderPlugin.java
@@ -1,53 +1,97 @@
 package com.breakreminder;
 
 import com.google.inject.Provides;
-import javax.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
+import lombok.Getter;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
-@Slf4j
+import javax.inject.Inject;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @PluginDescriptor(
-	name = "Example"
+        name = "Break Reminder",
+        description = "Reminder to take regular breaks"
 )
-public class BreakReminderPlugin extends Plugin
-{
-	@Inject
-	private Client client;
+public class BreakReminderPlugin extends Plugin {
 
-	@Inject
-	private BreakReminderConfig config;
+    @Inject
+    private OverlayManager overlayManager;
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
-	}
+    @Inject
+    private BreakOverlay breakOverlay;
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
-	}
+    @Inject
+    private BreakConfig config;
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
+    @Provides
+    BreakConfig provideConfig(ConfigManager configManager) { return configManager.getConfig(BreakConfig.class); }
 
-	@Provides
-	BreakReminderConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(BreakReminderConfig.class);
-	}
+    @Getter
+    private Timer timer;
+
+    @Getter
+    private int currentBreakIntervalLength;
+
+    private final List<GameState> previousGameStates = new ArrayList<>();
+
+    private final int hoppingStatesQuantity = 3;
+
+    @Override
+    protected void startUp()
+    {
+        currentBreakIntervalLength = config.nextBreakInterval();
+
+        long intervalInSeconds = currentBreakIntervalLength * 60L;
+        Instant intervalEndTime = Instant.now().plusSeconds(intervalInSeconds);
+
+        timer = new Timer(intervalEndTime);
+        overlayManager.add(breakOverlay);
+    }
+
+    @Override
+    protected void shutDown()
+    {
+        overlayManager.remove(breakOverlay);
+        previousGameStates.clear();
+        timer = null;
+    }
+
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged event)
+    {
+        previousGameStates.add(event.getGameState());
+
+        if (previousGameStates.size() < hoppingStatesQuantity)
+        {
+            return;
+        }
+
+        if (event.getGameState() == GameState.LOGGED_IN && !isWorldHopping())
+        {
+            currentBreakIntervalLength = config.nextBreakInterval();
+
+            int intervalInSeconds = config.nextBreakInterval() * 60;
+            Instant intervalEndTime = Instant.now().plusSeconds(intervalInSeconds);
+            
+            timer = new Timer(intervalEndTime);
+        }
+    }
+
+    private boolean isWorldHopping()
+    {
+        int size = previousGameStates.size();
+        List<GameState> states = previousGameStates.subList(size - hoppingStatesQuantity, size);
+
+        return     states.get(0) == GameState.HOPPING
+                && states.get(1) == GameState.LOADING
+                && states.get(2) == GameState.LOGGED_IN;
+    }
 }
